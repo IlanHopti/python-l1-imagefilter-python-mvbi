6944df9d5ebe3af447c6e21cb0f1de9a0dad6359
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2021-12-08 15:11:21 +0100

    Call in main and fix try and except

--- filter/dilatation.py
+++ filter/dilatation.py
@@ -2,25 +2,28 @@ import numpy as np
 import cv2
 
 
-img = cv2.imread('../img/juan.jpg', 1)
-
-try:
-    cv2.imshow('Original', img)
-
-    kernel = np.ones((5, 5), 'uint8')
-
-    dilate_img = cv2.dilate(img, kernel, iterations=1)
-    cv2.imshow('Dilated Image', dilate_img)
-    cv2.waitKey(10000)
-    cv2.destroyAllWindows()
-except cv2.error as e:
-    print("Image not found")
-
-try:
-    result = cv2.imwrite(r'../img/output/blurJuan.jpg', dilate_img)
-    if result:
-        print("File saved successfully")
+def dilatation(le_lien):
+    img = cv2.imread(f'./img/{le_lien}', 1)
+    if not le_lien.endswith('.jpg' or '.png'):
+        print("Is not a jpg or a png")
     else:
-        print("Error in saving file")
-except NameError as n:
-    print("Wrong directory")
+        try:
+            cv2.imshow('Original', img)
+
+            kernel = np.ones((5, 5), 'uint8')
+
+            dilate_img = cv2.dilate(img, kernel, iterations=1)
+            cv2.imshow('Dilated Image', dilate_img)
+            cv2.waitKey(3000)
+            cv2.destroyAllWindows()
+        except cv2.error as e:
+            print("Image not found")
+
+        try:
+            result = cv2.imwrite(r'./img/output/blurJuan.jpg', dilate_img)
+            if result:
+                print("File saved successfully")
+            else:
+                print("Error in saving file")
+        except NameError as n:
+            print("Wrong directory")
--- filter/gaussian_blur.py
+++ filter/gaussian_blur.py
@@ -1,25 +1,30 @@
 import cv2
 import numpy
 
-src = cv2.imread('../img/juan.jpg', cv2.IMREAD_UNCHANGED)
-ksize1 = 9
-ksize2 = 9
+def blur(le_lien):
 
-try:
-    dst = cv2.GaussianBlur(src, (ksize1, ksize2), cv2.BORDER_DEFAULT)
+    src = cv2.imread('./img/juan.jpg', cv2.IMREAD_UNCHANGED)
 
-    cv2.imshow("Gaussian Smoothing", numpy.hstack((src, dst)))
-
-    cv2.waitKey(10000)
-    cv2.destroyAllWindows()
-except cv2.error as e:
-    print("Image not found")
-
-try:
-    result = cv2.imwrite(r'../img/output/blurJuan.jpg', dst)
-    if result:
-        print("File saved successfully")
+    if not le_lien.endswith('.jpg' or '.png'):
+        print("Is not a jpg or a png")
     else:
-        print("Error in saving file")
-except NameError as n:
-    print("Wrong directory")
+        ksize1 = 9
+        ksize2 = 9
+        try:
+            dst = cv2.GaussianBlur(src, (ksize1, ksize2), cv2.BORDER_DEFAULT)
+
+            cv2.imshow("Gaussian Smoothing", numpy.hstack((src, dst)))
+
+            cv2.waitKey(3000)
+            cv2.destroyAllWindows()
+        except cv2.error as e:
+            print("Image not found")
+
+        try:
+            result = cv2.imwrite(r'./img/output/blurJuan.jpg', dst)
+            if result:
+                print("File saved successfully")
+            else:
+                print("Error in saving file")
+        except NameError as n:
+            print("Wrong directory")
