1d9b34a3990d82f966b25fad3a0650cfdfe909a7
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2021-12-09 13:00:35 +0100

    ⚙️-Move main's content tout cli.py

--- cli.py
+++ cli.py
@@ -1,11 +1,35 @@
 import sys
-import main
+
+from filter.dilatation import Dilatation
+from filter.gaussian_blur import Blur
+from filter.grayscale import Gray
+import cv2
+import os
+
+
+def Start():
+    list = os.listdir("img")
+
+    for img in list:
+        if not img.endswith('.jpg' or '.png'):
+            print("Is not a jpg or a png")
+        else:
+            img_path = f'{input_dir}/{img}'
+            image = cv2.imread(img_path)
+            image = Gray(image)
+            image = Dilatation(image)
+            image = Blur(image)
+
+            output = f'{output_dir}/{img}'
+            cv2.imwrite(output, image)
+
+            print(" Image successfully uploaded")
 
 args = sys.argv
 
 for i, a in enumerate(args):
 
-    if a == '-h'or a == '--help':
+    if a == '-h' or a == '--help':
         print("usage: imagefilter\n"
               "-h, --help :\n"
               "-i,--input-dir <directory>\n"
@@ -13,10 +37,17 @@ for i, a in enumerate(args):
 
     elif a == '-i' or a == '--input-dir':
         input_dir = args[i + 1]
-        #mettre input_dir dans img du main
+        # mettre input_dir dans img du main
         print(input_dir)
 
     elif a == '-o' or a == '--output-dir':
         output_dir = args[i + 1]
-        #mettre output_dir dans le fichier output du main
+        # mettre output_dir dans le fichier output du main
         print(output_dir)
+
+    elif a == '-s' or a == '--start':
+        Start()
+
+
+
+
--- main.py
+++ main.py
@@ -2,24 +2,24 @@
 from filter.dilatation import Dilatation
 from filter.gaussian_blur import Blur
 from filter.grayscale import Gray
-import cv2
-import os
-
-
-list = os.listdir("img")
-
-
-for img in list:
-    if not img.endswith('.jpg' or '.png'):
-        print("Is not a jpg or a png")
-    else:
-        img_path = f'img/{img}'
-        image = cv2.imread(img_path)
-        image = Gray(image)
-        image = Dilatation(image)
-        image = Blur(image)
-
-        output = f'output/{img}'
-        cv2.imwrite(output, image)
-
-        print(" Image successfully uploaded")
+# import cv2
+# import os
+#
+#
+# list = os.listdir("img")
+#
+#
+# for img in list:
+#     if not img.endswith('.jpg' or '.png'):
+#         print("Is not a jpg or a png")
+#     else:
+#         img_path = f'img/{img}'
+#         image = cv2.imread(img_path)
+#         image = Gray(image)
+#         image = Dilatation(image)
+#         image = Blur(image)
+#
+#         output = f'output/{img}'
+#         cv2.imwrite(output, image)
+#
+#         print(" Image successfully uploaded")
