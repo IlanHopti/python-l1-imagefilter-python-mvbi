7cbb89893593520f3dbc15c21b554ce34ef0ab07
Maxime Nicolas <71704827+Maxime-UwU@users.noreply.github.com>
2021-12-10 10:56:13 +0100

    png to gif converter

--- filter/grayscale.py
+++ filter/grayscale.py
@@ -6,7 +6,7 @@ import logger
 def Gray(image):
     try:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        logger.log("Application of Grayscale filter")
+        logger.log("Application of G@rayscale filter")
         return gray
 
     except cv2.error:
